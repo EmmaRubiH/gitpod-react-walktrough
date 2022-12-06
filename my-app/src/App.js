ca5b9314585837142c713aa78b7ac717e4f837aa
EmmaRubiH <emmahjorting89@gmail.com>
2022-12-06 14:04:39 +0000

    add new components

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,19 +1,30 @@
-import logo from './logo.svg';
 import './App.css';
-import FunctionalGreeting from './components/FunctionalGreeting';
-import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-import StatefulGreeting from './components/StatefulGreeting';
-import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import NestingComponents from './components/NestingComponents';
+// import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+// import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+// import EventBinding from './components/EventBinding';
+// import EventsClass from './components/EventsClass';
+// import EventsFunctional from './components/EventsFunctional';
+// import FunctionalGreeting from './components/FunctionalGreeting';
+// import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+// import StatefulGreeting from './components/StatefulGreeting';
+// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
-      <StatefulGreetingWithPrevState />
+      <NestingComponents />
+      {/* <ConditionalRenderingClass />
+      <ConditionalRenderingFunctional connected={true}/> */}
+    {/* <EventBinding /> */}
+      {/* // <EventsClass />
+      // <EventsFunctional /> */}
+      {/* /* <StatefulGreetingWithPrevState />
       <StatefulGreetingWithCallback />
       <StatefulGreeting greeting="I'm a stateful class component!" name="Mike" />
       <FunctionalGreeting />
-      <FunctionalGreetingWithProps greeting="nice to meet you" name="Mike" age="32" />
+      <FunctionalGreetingWithProps greeting="nice to meet you" name="Mike" age="32" /> */ }
     </div>
   );
 }
--- my-app/src/components/FunctionalGreetingWithProps.js
+++ my-app/src/components/FunctionalGreetingWithProps.js
@@ -1,9 +1,9 @@
-import React from "react";
+// import React from "react";
 
 
-const FunctionalGreetingWithProps = (props) => {
-    console.log(props);
-    return <h1>Hello, {props.name}! I see you're {props.age} years old; {props.greeting} !!</h1>;
-}
+// const FunctionalGreetingWithProps = (props) => {
+//     console.log(props);
+//     return <h1>Hello, {props.name}! I see you're {props.age} years old; {props.greeting} !!</h1>;
+// }
 
-export default FunctionalGreetingWithProps;
+// export default FunctionalGreetingWithProps;
