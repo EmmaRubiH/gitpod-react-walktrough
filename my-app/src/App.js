87c0772e05a0be028c5b336a8100aef3b7d589f8
EmmaRubiH <emmahjorting89@gmail.com>
2022-12-09 09:57:38 +0000

    add new component and work with mounting

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,22 +1,28 @@
 import './App.css';
-import RenderingLists from './components/RenderingLists';
-// import MethodsAsPropsParent from './components/MethodsAsPropsParent';
-// import NestingComponents from './components/NestingComponents';
-// import ConditionalRenderingClass from './components/ConditionalRenderingClass';
-// import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
-// import EventBinding from './components/EventBinding';
-// import EventsClass from './components/EventsClass';
-// import EventsFunctional from './components/EventsFunctional';
-// import FunctionalGreeting from './components/FunctionalGreeting';
-// import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
-// import StatefulGreeting from './components/StatefulGreeting';
-// import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
-// import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
+import LifeCyclesCWU from './components/LifeCyclesCWU';
+// import LifeCyclesCDU from './components/LifeCyclesCDU';
+// import LifeCyclesCDM from './components/LifeCyclesCDM';
+// import RenderingLists from './components/RenderingLists';
+// // import MethodsAsPropsParent from './components/MethodsAsPropsParent';
+// // import NestingComponents from './components/NestingComponents';
+// // import ConditionalRenderingClass from './components/ConditionalRenderingClass';
+// // import ConditionalRenderingFunctional from './components/ConditionalRenderingFunctional';
+// // import EventBinding from './components/EventBinding';
+// // import EventsClass from './components/EventsClass';
+// // import EventsFunctional from './components/EventsFunctional';
+// // import FunctionalGreeting from './components/FunctionalGreeting';
+// // import FunctionalGreetingWithProps from './components/FunctionalGreetingWithProps';
+// // import StatefulGreeting from './components/StatefulGreeting';
+// // import StatefulGreetingWithCallback from './components/StatefulGreetingWithCallback';
+// // import StatefulGreetingWithPrevState from './components/StatefulGreetingWithPrevState';
 
 function App() {
   return (
     <div className="App">
-      <RenderingLists />
+      <LifeCyclesCWU />
+      {/* <LifeCyclesCDU /> */}
+      {/* <LifeCyclesCDM /> */}
+      {/* <RenderingLists />
       {/* <MethodsAsPropsParent /> */}
       {/* <NestingComponents /> */}
       {/* <ConditionalRenderingClass />
